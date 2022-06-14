96e5779e72b59f41fac2d5ad53eee55361a76f1a
alejandritoo <107460974+alejandritoo@users.noreply.github.com>
2022-06-14 10:21:16 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showIcon(IconNames.Heart)
+basic.pause(100)
+basic.showNumber(4)
+basic.pause(100)
+basic.showNumber(3)
+basic.pause(100)
+basic.showNumber(2)
+basic.pause(100)
+basic.showNumber(1)
+basic.pause(100)
+basic.showNumber(0)
+basic.forever(function () {
+	
+})
